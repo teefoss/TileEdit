92bdfe54986939e950daed99344d45c5d58ca9a5
xiphiasnonus <foster.pianist@gmail.com>
2018-08-02 19:14:22 -0400

    Initial Commit

---
+++ TileEdit/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TileEdit
+//
+//  Created by Thomas Foster on 8/2/18.
+//  Copyright © 2018 Thomas Foster. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
